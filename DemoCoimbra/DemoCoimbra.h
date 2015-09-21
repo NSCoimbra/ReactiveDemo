7eeb5b47323185e130d29a874949cb99ba3352bb
Rui Peres <rui.peres@mailonline.co.uk>
2015-09-21 23:04:32 +0100

    Initial Commit

---
+++ DemoCoimbra/DemoCoimbra.h
@@ -0,0 +1,19 @@
+//
+//  DemoCoimbra.h
+//  DemoCoimbra
+//
+//  Created by Rui Peres on 21/09/2015.
+//  Copyright © 2015 Aphely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DemoCoimbra.
+FOUNDATION_EXPORT double DemoCoimbraVersionNumber;
+
+//! Project version string for DemoCoimbra.
+FOUNDATION_EXPORT const unsigned char DemoCoimbraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoCoimbra/PublicHeader.h>
+
+
