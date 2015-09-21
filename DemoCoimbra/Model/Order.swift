4f091ac53c5a43154240b93aab0972957b255dc5
Rui Peres <rui.peres@mailonline.co.uk>
2015-09-21 23:04:54 +0100

    Added the Model

---
+++ DemoCoimbra/Model/Order.swift
@@ -0,0 +1,16 @@
+//
+//  Order.swift
+//  DemoCoimbra
+//
+//  Created by Rui Peres on 21/09/2015.
+//  Copyright © 2015 Aphely. All rights reserved.
+//
+
+import Foundation
+
+struct Order {
+    
+    let orderID: Int
+    let orderValue: Double
+    let orderDescription: String
+}
